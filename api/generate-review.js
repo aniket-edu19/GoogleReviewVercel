70571d42253f9dcf612533961d96fd6a80fb58fc
AniketEdu <180860131009@laxmi.edu.in>
2026-04-26 12:55:34 +0530

    Updated Gemini Model to 2.5 flash lite

--- api/generate-review.js
+++ api/generate-review.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   const API_KEY = process.env.GEMINI_API_KEY;
 
   try {
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`, {
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent?key=${API_KEY}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
